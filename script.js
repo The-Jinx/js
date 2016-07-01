5e3bd691461dbc65acc94c015ce8982d80a6eec1
Andrew <andyukr@i.ua>
2016-07-01 21:41:14 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-      
-function calculate(){ 
-        var a=prompt("enter value of a","");
+           var a=prompt("enter value of a","");
          var b=prompt("enter value of b","");
          var c=prompt("enter value of c","");   
         var disc=Math.sqrt(b*b-4*a*c);
@@ -9,10 +7,5 @@ function calculate(){
         var root2=(-b-disc)/denominator;
        document.write("first root is:"+root1+"<br>");
         document.write("second root is:"+root2+"<br>");   
-        }
         
-function smth(g1,g2){
-    var g1=10;
-    var g2=20;
-    document.write(g1*g2);
-}
+        
