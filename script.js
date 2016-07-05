c9c4bb70e792b353fd8414d3ae59cedf70f69cc6
Andrew <andyukr@i.ua>
2016-07-05 10:28:40 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-           var a=prompt("enter value of a","");
-         var b=prompt("enter value of b","");
-         var c=prompt("enter value of c","");   
-        var disc=Math.sqrt(b*b-4*a*c);
-        var denominator= 2*a;
-        var root1=(-b+disc)/denominator;
-        var root2=(-b-disc)/denominator;
-       document.write("first root is:"+root1+"<br>");
-        document.write("second root is:"+root2+"<br>");   
-        
-        
+var fib = [];
+var n = prompt("Enter number of loops","0");
+
+fib[0] = 1;
+fib[1] = 1;
+for(var i=2; i<=n; i++)
+{
+    fib[i] = fib[i-2] + fib[i-1];
+}
+alert(fib[n]);
